ea0804feaf2b4544da281de80b31560628a8c4b2
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-07-15 04:24:17 +0000

    style: update base and index st

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,8 @@
-<script setup lang="ts"></script>
 <template>
-  <div class="bg-gray-100 h-screen w-screen">
-    <h1 class="text-center text-4xl font-bold text-orange-500 py-10"> Vue.js Forge - Orange Lemur </h1>
-    <BoardsList></BoardsList>
+  <div class="max-w-screen-xl mx-auto">
+    <h1 class="text-center text-4xl font-bold text-orange-500 py-10">
+      Vue.js Forge - Orange Lemur
+    </h1>
+    <BoardsGrid></BoardsGrid>
   </div>
 </template>
