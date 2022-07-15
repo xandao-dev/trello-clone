23b202809b82cf5f4e595529c47d9880593ff587
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-07-15 05:11:48 +0000

    feat: add initial sidebar

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,28 @@
 <template>
-  <div class="max-w-screen-xl mx-auto">
-    <h1 class="text-center text-4xl font-bold text-orange-500 py-10">
-      Vue.js Forge - Orange Lemur
-    </h1>
-    <BoardsGrid></BoardsGrid>
+  <div class="grid-template">
+    <side-bar class="template-side-bar" />
+    <div class="template-app max-w-screen-xl mx-auto w-full">
+      <h1 class="text-center text-4xl font-bold text-orange-500 py-10">
+        Vue.js Forge - Orange Lemur
+      </h1>
+      <boards-grid></boards-grid>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.grid-template {
+  display: grid;
+  grid-template-columns: min-content auto;
+  grid-template-rows: 1fr;
+  grid-template-areas: "side-bar app";
+}
+
+.grid-template > .template-side-bar {
+  grid-area: side-bar;
+}
+
+.grid-template > .template-app {
+  grid-area: app;
+}
+</style>
